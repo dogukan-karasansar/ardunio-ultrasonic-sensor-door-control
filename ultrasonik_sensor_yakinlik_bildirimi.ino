9415eaa3ed3b7dd0e2212d2ada97474609760111
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-11-26 14:57:06 +0300

    ultrasonic sensörlü potansiyometre kontrollü kapıda bekleyen projesi

---
+++ ultrasonik_sensor_yakinlik_bildirimi.ino
@@ -0,0 +1,139 @@
+// wifi
+const char* ssid =  "roottv";
+const char* pswrd = "u4U0xvGh";
+
+//Sensör 1
+#define echoPin 6
+#define trigPin 7
+
+//Buzzer
+#define buzzerPin 8
+
+//Sensör 2
+#define echoPin2  9
+#define trigPin2  10
+
+//potansiyometre
+#define potansiyo A0
+int potansiyoValue = 0;
+
+
+int maximumRange = 50, minimumRange = 0;
+//sayaç
+int count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(echoPin2, INPUT);
+
+}
+
+//ardunio reset fonksiyonu
+void(* resetFunc) (void) = 0;
+
+void loop() {
+
+  //potansiyo okuma
+  potansiyoValue = analogRead(potansiyo);
+  //bu işlem ile potansiyometre en yüksek ve en az gerilimini alarak hesaplayabiliriz
+  potansiyoValue = (5.00 / 1024.00) * potansiyoValue;
+  Serial.println(potansiyoValue);
+
+  if (potansiyoValue == 4) {
+
+    //başlangıç bekleyişi kısa için 2 uzun için 10 yazabilirsin
+    delay(2000);
+
+    //ölcümler
+    int olcum = mesafe(maximumRange, minimumRange);
+    int olcum2 = mesafe2(maximumRange, minimumRange);
+
+    if (olcum > 0 || olcum2 > 0) {
+      count++;
+      //count 2 * 3 yani = 6 sn boyunca olcum 0 dan büyük önünde engel varsa melodiyi çaldır
+      if (count >= 3) {
+        melodi(olcum * 10);
+        melodi(olcum2 * 10);
+      }
+
+    } else {
+      //olcum 0 sa ardunioyu resetle
+      resetFunc();
+    }
+
+  }
+}
+
+
+
+int mesafe(int maximumRange, int minimumRange) {
+  long duration, distance;
+
+  digitalWrite(trigPin, LOW);
+
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  duration = pulseIn(echoPin, HIGH);
+
+  /*Serial.print("Sensor 1: ");
+    Serial.print((duration / 29) / 2);
+    Serial.print("CM");
+    Serial.println();*/
+
+  distance = (duration / 29.1) / 2;
+
+  delay(50);
+
+
+  if (distance >= maximumRange || distance <= minimumRange)
+    return 0;
+  return distance;
+}
+
+int mesafe2(int maximumRange, int minimumRange) {
+  long duration, distance;
+
+  digitalWrite(trigPin2, LOW);
+
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+
+
+  duration = pulseIn(echoPin2, HIGH);
+
+  /*Serial.print("Sensor 2: ");
+    Serial.print((duration / 29) / 2);
+    Serial.print("CM");
+    Serial.println();*/
+
+  distance = (duration / 29.1) / 2;
+
+  delay(50);
+
+
+  if (distance >= maximumRange || distance <= minimumRange)
+    return 0;
+  return distance;
+}
+
+int melodi(int dly) {
+  tone(buzzerPin, 440);
+  delay(dly);
+  noTone(buzzerPin);
+  delay(dly);
+}
