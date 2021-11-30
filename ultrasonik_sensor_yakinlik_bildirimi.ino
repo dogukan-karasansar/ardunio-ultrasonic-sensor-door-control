d0bb6068b653712d6ecf4aae12bb5b8e59f8705f
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-11-30 09:23:51 +0300

    bluetooth connection system

--- ultrasonik_sensor_yakinlik_bildirimi.ino
+++ ultrasonik_sensor_yakinlik_bildirimi.ino
@@ -17,16 +17,28 @@ const char* pswrd = "u4U0xvGh";
 #define potansiyo A0
 int potansiyoValue = 0;
 
+//control led
+#define leftPotansyioLed 2
+#define rightPotansyioLed 3
+
+
+
 
 int maximumRange = 50, minimumRange = 0;
 //sayaç
 int count = 0;
+int flag = 0;
+char code;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(1000);
 
+  //ledler
+  pinMode(leftPotansyioLed, OUTPUT);
+  pinMode(rightPotansyioLed, OUTPUT);
 
+  //ultrasonic pinler
   pinMode(trigPin, OUTPUT);
   pinMode(trigPin2, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
@@ -37,17 +49,55 @@ void setup() {
 
 //ardunio reset fonksiyonu
 void(* resetFunc) (void) = 0;
+void ultraSonic() {
+  digitalWrite(leftPotansyioLed, LOW);
+  digitalWrite(rightPotansyioLed, HIGH);
+
+  //başlangıç bekleyişi kısa için 2 uzun için 10 yazabilirsin
+  delay(2000);
+
+  //ölcümler
+  int olcum = mesafe(maximumRange, minimumRange);
+  int olcum2 = mesafe2(maximumRange, minimumRange);
+
+  if (olcum > 0 || olcum2 > 0) {
+    count++;
+    //count 2 * 3 yani = 6 sn boyunca olcum 0 dan büyük önünde engel varsa melodiyi çaldır
+    if (count >= 3) {
+      Serial.println(count);
+      melodi(olcum * 10);
+      melodi(olcum2 * 10);
+    }
+  } else {
+    //olcum 0 sa ardunioyu resetle
+    resetFunc();
+  }
+}
 
 void loop() {
 
-  //potansiyo okuma
-  potansiyoValue = analogRead(potansiyo);
-  //bu işlem ile potansiyometre en yüksek ve en az gerilimini alarak hesaplayabiliriz
-  potansiyoValue = (5.00 / 1024.00) * potansiyoValue;
-  Serial.println(potansiyoValue);
+  //bluetooth dan gelen değer
+  if (Serial.available()) {
+    char code = Serial.read();
+    Serial.println(code);
+    if (code == 'f') {
+      flag = 1;
+    }
+    else {
+      flag = 0;
+    }
+  }
+  if (flag == 0) {
+    ultraSonic();
+  } else {
+    digitalWrite(leftPotansyioLed, HIGH);
+    digitalWrite(rightPotansyioLed, LOW);
+  }
 
-  if (potansiyoValue == 4) {
 
+  /*if (code == 'o') {
+    digitalWrite(leftPotansyioLed, LOW);
+    digitalWrite(rightPotansyioLed, HIGH);
     //başlangıç bekleyişi kısa için 2 uzun için 10 yazabilirsin
     delay(2000);
 
@@ -56,6 +106,7 @@ void loop() {
     int olcum2 = mesafe2(maximumRange, minimumRange);
 
     if (olcum > 0 || olcum2 > 0) {
+      Serial.println(count);
       count++;
       //count 2 * 3 yani = 6 sn boyunca olcum 0 dan büyük önünde engel varsa melodiyi çaldır
       if (count >= 3) {
@@ -68,7 +119,70 @@ void loop() {
       resetFunc();
     }
 
-  }
+
+    } else if (code == 'f') {
+    digitalWrite(leftPotansyioLed, HIGH);
+    digitalWrite(rightPotansyioLed, LOW);
+    } else {
+    //başlangıç bekleyişi kısa için 2 uzun için 10 yazabilirsin
+    delay(2000);
+
+    //ölcümler
+    int olcum = mesafe(maximumRange, minimumRange);
+    int olcum2 = mesafe2(maximumRange, minimumRange);
+
+    if (olcum > 0 || olcum2 > 0) {
+      Serial.println(count);
+      count++;
+      //count 2 * 3 yani = 6 sn boyunca olcum 0 dan büyük önünde engel varsa melodiyi çaldır
+      if (count >= 3) {
+        melodi(olcum * 10);
+        melodi(olcum2 * 10);
+      }
+
+    } else {
+      //olcum 0 sa ardunioyu resetle
+      resetFunc();
+    }
+    }*/
+
+
+  /*else {
+    //potansiyo okuma
+    potansiyoValue = analogRead(potansiyo);
+    //bu işlem ile potansiyometre en yüksek ve en az gerilimini alarak hesaplayabiliriz
+    potansiyoValue = (5.00 / 1024.00) * potansiyoValue;
+
+    if (potansiyoValue < 4)
+      digitalWrite(leftPotansyioLed, HIGH);
+    digitalWrite(rightPotansyioLed, LOW);
+    if (potansiyoValue == 4) {
+      digitalWrite(leftPotansyioLed, LOW);
+      digitalWrite(rightPotansyioLed, HIGH);
+      //başlangıç bekleyişi kısa için 2 uzun için 10 yazabilirsin
+      delay(2000);
+
+      //ölcümler
+      int olcum = mesafe(maximumRange, minimumRange);
+      int olcum2 = mesafe2(maximumRange, minimumRange);
+
+      if (olcum > 0 || olcum2 > 0) {
+        count++;
+        //count 2 * 3 yani = 6 sn boyunca olcum 0 dan büyük önünde engel varsa melodiyi çaldır
+        if (count >= 3) {
+          melodi(olcum * 10);
+          melodi(olcum2 * 10);
+        }
+
+      } else {
+        //olcum 0 sa ardunioyu resetle
+        resetFunc();
+      }
+
+    }
+    }*/
+
+
 }
 
 
